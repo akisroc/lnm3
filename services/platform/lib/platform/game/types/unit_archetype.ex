f2db93d4827d2937ca5e845ce23a1659a083609f
Adrien-H <adrien.h@tuta.io>
2026-01-10 14:05:11 +0100

    fixup! Add battles on Platform API

--- services/platform/lib/platform/game/types/unit_archetype.ex
+++ services/platform/lib/platform/game/types/unit_archetype.ex
@@ -1,4 +1,32 @@
 defmodule Platform.Game.UnitArchetype do
+  @moduledoc """
+  Defines the static attributes for unit archetypes.
+
+  ### Kill rate
+
+  **Kill rate** of an archetype is defined as the **ratio of power to
+  defense**. This quotient is hardcoded for performance purposes.
+
+  **Example (B1):**
+
+  `4.0 / 7.0 = 0.5714285714285714`, so let’s make it `0.57`.
+
+  ### Fame cost
+
+  **Fame cost** is derived from old LNM data. Soldiers were recruited in
+  barracks. Each barrack of a certain archetype occupied a certain number
+  of hectares, and allowed to recruit a certain number of soldiers. In this
+  new version, barracks are removed, so soldiers are directly recruited. But
+  the hectares were a key resource of LNM’s strategical aspects, so the
+  mechanism has been translated into the newly introduced **fame** resource.
+
+  **Example (B1):**
+
+  B1 barracks required 100 hectares to build and allowed to recruit 35 soldiers.
+  So each B1 soldier required in fact `100 / 35 = 2.857142857142857` hectares to
+  recruit. So let’s round it to `2.86` for its individual fame cost.
+  """
+
   defstruct [
     :label,
     :power,
@@ -48,6 +76,98 @@ defmodule Platform.Game.UnitArchetype do
   end
 
   defp b1 do
-    %__MODULE__{label: :b1, power: 4.0, defense: 7.0, speed: 85.0, kill_rate: 4.0 / 7.0, distance?: false, fame_cost: 2.86}
+    %__MODULE__{
+      label: :b1,
+      power: 4.0,
+      defense: 7.0,
+      speed: 85.0,
+      kill_rate: 0.57,
+      distance?: false,
+      fame_cost: 2.86
+    }
+  end
+
+  defp b1 do
+    %__MODULE__{
+      label: :b1,
+      power: 4.0,
+      defense: 7.0,
+      speed: 85.0,
+      kill_rate: 0.57,
+      distance?: false,
+      fame_cost: 2.86
+    }
+  end
+
+  defp b1 do
+    %__MODULE__{
+      label: :b1,
+      power: 4.0,
+      defense: 7.0,
+      speed: 85.0,
+      kill_rate: 0.57,
+      distance?: false,
+      fame_cost: 2.86
+    }
+  end
+
+  defp b1 do
+    %__MODULE__{
+      label: :b1,
+      power: 4.0,
+      defense: 7.0,
+      speed: 85.0,
+      kill_rate: 0.57,
+      distance?: false,
+      fame_cost: 2.86
+    }
+  end
+
+  defp b1 do
+    %__MODULE__{
+      label: :b1,
+      power: 4.0,
+      defense: 7.0,
+      speed: 85.0,
+      kill_rate: 0.57,
+      distance?: false,
+      fame_cost: 2.86
+    }
+  end
+
+  defp b1 do
+    %__MODULE__{
+      label: :b1,
+      power: 4.0,
+      defense: 7.0,
+      speed: 85.0,
+      kill_rate: 0.57,
+      distance?: false,
+      fame_cost: 2.86
+    }
+  end
+
+  defp b1 do
+    %__MODULE__{
+      label: :b1,
+      power: 4.0,
+      defense: 7.0,
+      speed: 85.0,
+      kill_rate: 0.57,
+      distance?: false,
+      fame_cost: 2.86
+    }
+  end
+
+  defp b1 do
+    %__MODULE__{
+      label: :b1,
+      power: 4.0,
+      defense: 7.0,
+      speed: 85.0,
+      kill_rate: 0.57,
+      distance?: false,
+      fame_cost: 2.86
+    }
   end
 end
